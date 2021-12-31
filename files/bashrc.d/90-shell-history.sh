f363d28c416ffd4543de938c25c03d728881ef44
Glenn Y. Rolland <glenux@glenux.net>
2021-12-31 13:00:36 +0100

    Do not share history between sessions

--- files/bashrc.d/90-shell-history.sh
+++ files/bashrc.d/90-shell-history.sh
@@ -1,3 +1,10 @@
 
+export HISTSIZE=10000
+export HISTFILESIZE=10000
+
 # don't put duplicate lines in the history. See bash(1) for more options
-export HISTCONTROL=ignoredups
+export HISTCONTROL=ignoredups:erasedups
+shopt -s histappend
+# Reload history on each command
+#PROMPT_COMMAND="history -n; history -w; history -c; history -r; $PROMPT_COMMAND"
+
